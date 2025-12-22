9d515cc542cf2f5ae8e77305acffee3e3f73e26d
许涛洋 <16265081+xu-taoyang1@user.noreply.gitee.com>
2025-12-22 08:50:25 +0800

    12月22日练习的代码

--- Python练习/Python100天进阶计划/07_07_生成二维数组i乘j.py
+++ Python练习/Python100天进阶计划/07_07_生成二维数组i乘j.py
@@ -3,6 +3,16 @@
 # 作者：______（填你的名字）
 
 # 在这里写你的代码
+x_str, y_str = input().strip().split(',')
+X = int(x_str)
+Y = int(y_str)
+maxrix = []
+for i in range(X):
+    row = []
+    for j in range(Y):
+        row.append(i*j)
+    maxrix.append(row)
+print(maxrix)
 
 # 测试代码
 # if __name__ == '__main__':
